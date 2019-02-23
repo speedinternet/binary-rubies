7ed40cfcff0178c6ed54c6e296464e58f7f4c420
root <root@build231.redhat.org>
2019-02-23 23:34:07 +0000

    updates build example script

--- scripts/build_parallel-example.sh
+++ scripts/build_parallel-example.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+#
+# This will parse files in versions directory and generate
+# binary rubies for the current OS. Once generated, the src
+# directory will be removed in order to save on hdd space
+#
+# TODO:
+#	- automatic generation of sumfiles
+#
 
-
-cat versions/ruby-versions.txt | nice -n -20 parallel -j+0 --eta 'nice -n -20 /usr/local/rvm/bin/rvm --debug install --skip-gemsets --movable {} && nice -n -20 /usr/local/rvm/bin/rvm --debug prepare --path {}; rm -rf /usr/local/rvm/src/ruby-{}'
+for RUBYVER_FILE in versions/*.txt; do
+	echo -e "\\033[1mProcessing ruby version file: ${RUBYVER_FILE}\\033[0m";
+	cat ${RUBYVER_FILE} | nice -n -20 parallel -j+0 --eta 'nice -n -20 /usr/local/rvm/bin/rvm --debug install --skip-gemsets --movable {} && nice -n -20 /usr/local/rvm/bin/rvm --debug prepare --path {}; rm -rf /usr/local/rvm/src/ruby-{}';
+done
