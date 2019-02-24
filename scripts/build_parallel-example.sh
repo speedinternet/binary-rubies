6c4bc7281e98cd4bf6a0af190d3fa7c75b428fc1
root <root@build231.redhat.org>
2019-02-24 02:34:57 +0000

    update build example file

--- scripts/build_parallel-example.sh
+++ scripts/build_parallel-example.sh
@@ -4,11 +4,24 @@
 # binary rubies for the current OS. Once generated, the src
 # directory will be removed in order to save on hdd space
 #
-# TODO:
-#	- automatic generation of sumfiles
-#
 
+# Get arch info
+OSARCH='uname -m';
+
+# Get release infos fomr /etc/os-release file
+[[ -f /etc/os-release ]] && source /etc/os-release
+
+# For each version file
 for RUBYVER_FILE in versions/*.txt; do
+
 	echo -e "\\033[1mProcessing ruby version file: ${RUBYVER_FILE}\\033[0m";
 	cat ${RUBYVER_FILE} | nice -n -20 parallel -j+0 --eta 'nice -n -20 /usr/local/rvm/bin/rvm --debug install --skip-gemsets --movable {} && nice -n -20 /usr/local/rvm/bin/rvm --debug prepare --path {}; rm -rf /usr/local/rvm/src/ruby-{}';
+
+	# Generate md5sum if not already existing
+	if [[ -d binaries/${ID}/${VERSION_ID}/${OSARCH} ]]; then
+		pushd binaries/${ID}/${VERSION_ID}/${OSARCH} &>/dev/null;
+			ls -1 *.tar.bz2 | xargs -n1 bash -c '[[ ! -f ${0}.md5 ]] && md5sum ${0} > ${0}.md5';
+		popd &>/dev/null;
+	fi
+
 done
